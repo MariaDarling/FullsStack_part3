6cbafeb630b494504dab803d32e11c26cd6bb566
MaríaDarlingSantiagoCruz <darling_santiago@anahuac.mx>
2026-09-21 00:52:35 -0600

    Ejercicios 3.9 a 3.11

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const morgan = require('morgan') 
+const morgan = require('morgan')
+const cors = require('cors') 
 const app = express()
 
+app.use(cors()) 
 app.use(express.json())
+app.use(express.static('dist')) 
+
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
